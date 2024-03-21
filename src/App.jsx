68e47e09c9760331f89bab4aac8863e041b0843d
vitor <hugov3141v@gmail.com>
2024-03-21 08:30:46 -0300

    a

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import CurrencyConverter from './components/CurrencyConverter'
+
+function App() {
+ 
+
+  return (
+    <>
+      <CurrencyConverter />
+    </>
+  )
+}
+
+export default App
